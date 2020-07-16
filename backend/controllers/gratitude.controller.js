ba5c4678596e11024a383697d08790f036999019
Ethan McMasters <emcmast@siue.edu>
2020-07-16 12:23:17 -0500

    add endpoints for gratitude CRUD operations

--- backend/controllers/gratitude.controller.js
+++ backend/controllers/gratitude.controller.js
@@ -1,7 +1,22 @@
 const db = require("../models");
+const { gratitude } = require("../models");
 const Gratitude = db.gratitude;
 
 require('dotenv').config();
+
+exports.getGratitudes = (req, res) => {
+    Gratitude.find()
+        .then(gratitudes => res.json(gratitudes))
+        .catch(err => res.status(400).json('Error: ' + err));
+    
+}
+
+exports.getGratitude = (req, res) => {
+    Gratitude.findById(req.params.id)
+        .then(gratitude => res.json(gratitude))
+        .catch(err => res.status(400).json('Error: ' + err));
+    
+}
 
 exports.addGratitude = (req, res) => {
     const username = req.body.username;
@@ -13,6 +28,25 @@ exports.addGratitude = (req, res) => {
     });
 
     newGratitude.save()
-    .then(() => res.json('Gratitude added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+        .then(() => res.json('Gratitude added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+}
+
+exports.deleteGratitude = (req, res) => {
+    Gratitude.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Gratitude deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+}
+
+exports.updateGratitude = (req, res) => {
+    Gratitude.findById(req.params.id)
+        .then(gratitude => {
+            gratitude.username = req.body.username;
+            gratitude.description = req.body.description;
+
+            gratitude.save()
+                .then(() => res.json('Gratitude updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
 }
